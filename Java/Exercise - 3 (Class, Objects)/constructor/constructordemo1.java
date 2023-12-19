d3c2cdb9da576d807861064977577c9a6a1154f5
Nischal <sainischalmv@gmail.com>
2023-12-19 07:21:48 +0530

    indentation -_-

--- Java/Exercise - 3 (Class, Objects)/constructor/constructordemo1.java
+++ Java/Exercise - 3 (Class, Objects)/constructor/constructordemo1.java
@@ -1,25 +1,23 @@
 //constructor with no parameters:
-class A
-{
-int l,b;
-A()
-{
-    l=10;
-    b=20;
+class A {
+    int l, b;
+
+    A() {
+        l = 10;
+        b = 20;
+    }
+
+    int area() {
+        return l * b;
+    }
 }
-int area()
-{
-return l*b;
-}
-}
-class constructordemo1
-{
-public static void main(String args[])
-{
-A a1=new A();
-int r=a1.area();
-System.out.println("The area is: "+r);
-}
+
+class constructordemo1 {
+    public static void main(String args[]) {
+        A a1 = new A();
+        int r = a1.area();
+        System.out.println("The area is: " + r);
+    }
 }
 // Output:
 // The area is:200
--- Java/Exercise - 3 (Class, Objects)/constructor/contructordemo2.java
+++ Java/Exercise - 3 (Class, Objects)/constructor/contructordemo2.java
@@ -1,25 +1,23 @@
 // (ii) A constructor with parameters
-class A
-{
-int l,b;
-A(int u,int v)
-{
-l=u;
-b=v;
+class A {
+    int l, b;
+
+    A(int u, int v) {
+        l = u;
+        b = v;
+    }
+
+    int area() {
+        return l * b;
+    }
 }
-int area()
-{
-return l*b;
-}
-}
-class constructordemo2
-{
-public static void main(String args[])
-{
-A a1=new A(10,20);
-int r=a1.area();
-System.out.println("The area is: "+r);
-}
+
+class constructordemo2 {
+    public static void main(String args[]) {
+        A a1 = new A(10, 20);
+        int r = a1.area();
+        System.out.println("The area is: " + r);
+    }
 }
 // Output:
 // Page 12
